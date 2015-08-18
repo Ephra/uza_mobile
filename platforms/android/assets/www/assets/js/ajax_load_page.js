efaa4829b94a0c0cf3813a2fef0151ac5d5ba7e9
joshua john <software.engineerdev@gmail.com>
2015-08-18 07:32:35 +0100

    I have added some changes in the upload pruduct file

---
+++ platforms/android/assets/www/assets/js/ajax_load_page.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function () {
+    $(".megamenu").megamenu();
+    loadPage('body.html');
+});
+loadPage = function (url) {
+    NProgress.start();
+    $.get(url, {pg: 'login', file: 'login'}, function (data) {
+        $('.body_content').html(data);
+    NProgress.done();
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+       
+
