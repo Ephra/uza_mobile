f524b2781ed2272dddcf4611db5b264823c7c5f3
Ephraim Swilla <swillae1@gmail.com>
2015-08-18 10:38:15 +0300

    format template and add functions for easy coding

--- www/assets/js/ajax_load_page.js
+++ www/assets/js/ajax_load_page.js
@@ -4,73 +4,13 @@ $(document).ready(function () {
     loadPage('body.html');
 });
 loadPage = function (url) {
-    $.get(url, {pg: 'login', file: 'login'}, function (data) {
-        $('.body_content').html(data);
+   // NProgress.start();
+    $.get(url, {null: null}, function (data) {
+	$('.body_content').html(data);
+	//NProgress.done();
     });
 }
 
-user_login = function () {
-
-    var user_email = $('#email').val();
-    var password = $('#password').val();
-
-    var userInput = 'email=' + user_email + '&password=' + user_password;
-
-    if (user_email == '' && password == '') {
-        $('.success').fadeOut(200).hide();
-        $('.error').fadeOut(200).show();
-
-    }
-
-    else {
-
-        $.ajax({
-            type: "POST",
-            url: "http://inetstz.com/uza_application/index.html",
-            data: userInput,
-            success: function () {
-                $('.success').fadeIn(200).show();
-                $('.error').fadeOut(200).hide();
-            }
-        });
-    }
-    return false;
-};
-
-
-
-user_register = function () {
-
-    var fname = $('#fname').val();
-    var lname = $('#lname').val();
-    var email = $('#email').val();
-    var password = $('#password').val();
-    var phone = $('#phone_number').val();
-    var location = $('#location').val();
-
-    var userInput = 'fname=' + user_fname + '&lname=' + user_lname + '&email=' + user_email + '&password=' + user_password + '&phone=' + user_phone + '&location=' + location;
-
-    if (fname == '' && lname == '' && email=='' && password==''&& phone=='' && location=='' ) {
-        $('.success').fadeOut(200).hide();
-        $('.error').fadeOut(200).show();
-
-    }
-
-    else {
-
-        $.ajax({
-            type: "POST",
-            url: "single_product.html",
-            data: userInput,
-            success: function () {
-                
-                $('.success').fadeIn(200).show();
-                $('.error').fadeOut(200).hide();
-            }
-        });
-    }
-    return false;
-};
 
 
 
@@ -81,4 +21,5 @@ user_register = function () {
 
 
 
-       
+
+
