967998ba9aa34a2b5809494f45f380ed88d53c3d
Erna <erna.exaud@gmail.com>
2015-08-19 15:06:52 +0300

    changes

--- www/assets/js/ajax_load_page.js
+++ www/assets/js/ajax_load_page.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
     $(".megamenu").megamenu();
     loadPage('body.html');
 });
-loadPage = function (url) {
+loadPage = (function (url) {
     NProgress.start();
     $.get(url, {pg: 'login', file: 'login'}, function (data) {
         $('.body_content').html(data);
     NProgress.done();
     });
-}
+});
 
 
 
